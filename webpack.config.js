ec734dd698a9692920303c3a314ecc6ba6c831a7
jzandag <zidrexandag10@gmail.com>
2021-01-17 16:30:23 +0800

    webpack configurations added with comments

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+
+const path = require('path')
+
+module.exports = {
+    mode: 'development',                        //tells webpack how it should bundle our code
+    entry: './src/index.js',                    //entry point of webpack
+    output: {
+        path: path.resolve(__dirname, 'dist'),  //path of the generated output
+        filename: 'bundle.js',                   //filename of generated file
+        publicPath: ''
+    },
+    devtool: 'cheap-module-eval-source-map'
+}

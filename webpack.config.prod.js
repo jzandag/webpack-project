287c24a07e045bf2c435761aecd8a6c88513b17e
jzandag <zidrexandag10@gmail.com>
2021-01-18 17:34:54 +0800

    webpack prod settings and script

---
+++ webpack.config.prod.js
@@ -0,0 +1,51 @@
+
+const path = require('path')
+const autoprefixer = require('autoprefixer')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'production',                        //tells webpack how it should bundle our code
+    entry: './src/index.js',                    //entry point of webpack
+    output: {
+        path: path.resolve(__dirname, 'dist'),  //path of the generated output
+        filename: 'bundle.js',                   //filename of generated file
+        publicPath: ''
+    },
+    devtool: 'none',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_module/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader', options: {
+                        importLoaders: 1,
+                        modules: {
+                            localIdentName: '[name]__[local]__[hash:base64:5]'
+                        }
+                    }},
+                    { loader: 'postcss-loader', options: {
+                        ident: 'postcss',
+                        plugins: () => [autoprefixer() ]
+                    }}
+                ] 
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'url-loader?limit=8000&name=images/[name].[ext]'
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: __dirname + '/src/index.html',                 //starting point for html plugin
+            inject: 'body'
+        })
+    ]
+}
